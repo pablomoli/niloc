ba23d2ae0cd030eef0bfd70b99f9ceb95aa0eb73
Pablo Molina <pablomolinarojas@gmail.com>
2025-09-02 10:28:38 -0400

    network

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ if __name__ == "__main__":
             db.session.commit()
             print("Created default admin user (username: pablo, password: 123)")
 
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
